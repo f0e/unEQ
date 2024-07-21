08036d2cf1b7346fd68541255768e06d1ce311fe
f0e <7321764+f0e@users.noreply.github.com>
2024-07-21 21:20:03 +1000

    feat: make --copy-meta a flag

--- uneq/cli.py
+++ uneq/cli.py
@@ -14,8 +14,10 @@ from .uneq import uneq
     required=True,
 )
 @click.option("-e", "--eq-path", type=click.Path(exists=True), help="EQ file path", required=True)
-@click.option("--copy-meta", type=bool, help="Copy file metadata (creation date, etc)")
-def cli(input: click.Path, eq_path: click.Path, copy_meta: bool = False):
+@click.option(
+    "--copy-meta", type=bool, is_flag=True, show_default=True, default=False, help="Copy file metadata (creation date, etc)"
+)
+def cli(input: click.Path, eq_path: click.Path, copy_meta: bool):
     uneq(Path(input), Path(eq_path), copy_meta)
 
 
